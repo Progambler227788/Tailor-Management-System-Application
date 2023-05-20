504f69eb34df24e0d5d61f8e3fcd40e9588d9730
Muhammad Talha Zaheer <zaheertalha123@users.noreply.github.com>
2023-05-20 16:34:52 +0500

    I set up the drawer

--- app/src/main/java/com/example/tailoringmanagement/HomeScreenActivity.kt
+++ app/src/main/java/com/example/tailoringmanagement/HomeScreenActivity.kt
@@ -1,11 +1,69 @@
 package com.example.tailoringmanagement
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.example.tailoringmanagement.databinding.ActivityHomeScreenBinding
 
 class HomeScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.drawerToolBar.toolBar)
+        binding.drawerToolBar.toolBar.setTitle(R.string.app_name)
+
+        val toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout,
+            binding.drawerToolBar.toolBar, R.string.drawer_open, R.string.drawer_close
+        )
+
+        toggle.syncState()
+        binding.drawerLayout.addDrawerListener(toggle)
+
+        binding.drawerNavView.setNavigationItemSelectedListener {
+            val id = it.itemId
+            when (id) {
+                R.id.drawerItemHome -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.drawerItemProfile -> {
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                }
+                R.id.drawerItemMyShop -> {
+                    Toast.makeText(this, "My Shop", Toast.LENGTH_SHORT).show()
+                }
+                R.id.drawerItemReportBug -> {
+                    Toast.makeText(this, "Report a Bug", Toast.LENGTH_SHORT).show()
+                }
+                R.id.drawerItemLogout -> {
+                    Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show()
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+    }
+
+    private fun launchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().add(R.id.frameLayoutContainer, fragment).commit()
     }
 }
